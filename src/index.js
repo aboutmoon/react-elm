dc3716dbdd082bdc70844b4d8fe1873935261b60
biao.he@kaiostech.com <biao.he@kaiostech.com>
2019-09-12 10:33:14 +0800

    手机端页面自适应解决方案

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import Router from './router'
 import './style/base.scss'
 import './images/svg-symbel.svg'
+import './config/rem'
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 
